1c8ae9c0c1ce168f266f1733b5facf3907bd3b5b
Matt Dennis <msd7734@rit.edu>
2016-09-21 21:28:20 -0400

    Add initial project files

---
+++ FTP.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csci351ftp
+{
+    
+
+    class FTP
+    {
+        // The prompt
+        public const string PROMPT = "FTP> ";
+
+        // Information to parse commands
+        public static readonly string[] COMMANDS = { "ascii",
+         "binary",
+         "cd",
+         "cdup",
+         "debug",
+         "dir",
+         "get",
+         "help",
+         "passive",
+         "put",
+         "pwd",
+         "quit",
+         "user" };
+
+        public const int ASCII = 0;
+        public const int BINARY = 1;
+        public const int CD = 2;
+        public const int CDUP = 3;
+        public const int DEBUG = 4;
+        public const int DIR = 5;
+        public const int GET = 6;
+        public const int HELP = 7;
+        public const int PASSIVE = 8;
+        public const int PUT = 9;
+        public const int PWD = 10;
+        public const int QUIT = 11;
+        public const int USER = 12;
+
+        // Help message
+
+        public static readonly String[] HELP_MESSAGE = {
+	"ascii      --> Set ASCII transfer type",
+	"binary     --> Set binary transfer type",
+	"cd <path>  --> Change the remote working directory",
+	"cdup       --> Change the remote working directory to the",
+        "               parent directory (i.e., cd ..)",
+	"debug      --> Toggle debug mode",
+	"dir        --> List the contents of the remote directory",
+	"get path   --> Get a remote file",
+	"help       --> Displays this text",
+	"passive    --> Toggle passive/active mode",
+    "put path   --> Transfer the specified file to the server",
+	"pwd        --> Print the working directory on the server",
+    "quit       --> Close the connection to the server and terminate",
+	"user login --> Specify the user name (will prompt for password" };
+
+        static void Main(string[] args)
+        {
+            Console.Write(HELP_MESSAGE);
+        }
+    }
+}
